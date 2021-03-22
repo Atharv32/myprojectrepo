37d9dd25243bf45642b1c67be01c8e686b482279
Atharv32 <55347236+Atharv32@users.noreply.github.com>
2021-03-22 18:32:44 +0530

    Update Calculator.java

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
 		// TODO Auto-generated method stub
 		add(10,40,30);
 	}
-	public static void add(int num1,int num2){
+	public static void add(int num1,int num2,int num3){
 	      System.out.println(num1+num2+num3);
 	  }
 	
