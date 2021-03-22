19282a8648b0fa830fbbe97ab376cf3b7b805267
Atharv32 <iamatharvshetty@gmail.com>
2021-03-22 18:30:15 +0530

    add code

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		// TODO Auto-generated method stub
-		add(10,30);
+		add(10,40);
 	}
 	public static void add(int num1,int num2){
 	      System.out.println(num1+num2);
