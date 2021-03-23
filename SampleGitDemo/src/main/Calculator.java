e957b1e0327c0863b28f35782b0ae3ada62ddb6b
Atharv32 <iamatharvshetty@gmail.com>
2021-03-23 09:30:44 +0530

    push

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -10,6 +10,8 @@ public class Calculator {
 	public static void add(int num1,int num2,int num3){
 	      System.out.println(num1+num2+num3);
 	  }
-	
+	public static void prod(int num1,int num2){
+	      System.out.println(num1*num2);
+	  }
 
 }
